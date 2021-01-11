821e0a499b68251e6687e6b2af39f5540bd0e21b
darwishzainstudio <darwishzainstudio@gmail.com>
2021-01-11 22:33:06 +0800

    11012021 2232

--- Saviour Robot/Assets/veryEasy/Script/playerMove.cs
+++ Saviour Robot/Assets/veryEasy/Script/playerMove.cs
@@ -17,14 +17,97 @@ public class playerMove : MonoBehaviour
     public float jumpForce;
 
     [Header("Boolean")]
-    public bool gameStart;
-    public bool gameWin;
+    public bool play;
+    public bool rise;
+    //public bool gameStart;
+    //public bool gameWin;
 
     [Header("Timer")]
     public float timer;
 
- 
+    [Header("Life")]
+    public GameObject[] heart;  
+    public int life; 
 
+    void Start()
+    {
+        thePlayer=GetComponent<Rigidbody2D>();
+        life=5;
+        startGame(life);
+    }
+    void Update()
+    {
+        if(play)
+        {
+            if((Input.GetMouseButtonDown(0) || Input.GetButtonDown("Jump")))
+            {
+                thePlayer.velocity = new Vector2(0, 1) * jumpForce * Time.deltaTime;   
+            }
+            if(transform.position.y>5||transform.position.y<-5)
+            {
+                minusLife();
+            }
+        }
+        if(rise)
+        {
+            elevate();
+        }
+    }
+    public void startGame(int theLife)
+    {
+        thePlayer.position = new Vector3(0,-7,0);
+        play=false;
+        rise=true;
+    }
+    void elevate()
+    {
+        thePlayer.velocity = new Vector2(0, 1) * jumpForce * Time.deltaTime;
+        if (transform.position.y >= 0)
+        {
+            StartCoroutine(theCoroutine());
+            theStatus.text = "Start";
+            rise=false;
+            startPlay();
+        }
+    }
+    void startPlay()
+    {
+        play=true;
+    }
+
+
+    void minusLife()
+    {
+        life--;
+        startGame(life);
+    }
+        private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.tag=="enemy")
+        {
+            minusLife();
+        }
+    }
+    IEnumerator theCoroutine()
+    {
+        while (true)
+        {
+            float timeSlip = Random.Range(0.5f, 2f);
+            yield return new WaitForSeconds(timeSlip);
+            float yCorona = Random.Range(3, 8);
+            float yDrone = Random.Range(-4, -8);
+
+            GameObject theDrone = Instantiate(drone, new Vector2(10, yDrone), transform.rotation);
+            GameObject theCorona = Instantiate(corona, new Vector2(12, yCorona), transform.rotation);
+
+            if(!play)
+            {
+                Destroy(theDrone.gameObject);
+                Destroy(theCorona.gameObject);
+            }
+        }
+    }
+    /*
     void Start()
     {
         thePlayer = GetComponent<Rigidbody2D>();
@@ -56,14 +139,7 @@ public class playerMove : MonoBehaviour
         {
             if (gameStart)
             {
-                thePlayer.velocity = new Vector2(0, 1) * jumpForce * Time.deltaTime;
-                if (transform.position.y >= 0)
-                {
-                    gameStart = false;
-                    StartCoroutine(theCoroutine());
-                    theStatus.text = "Start";
-                    //Debug.Log("Start");
-                }
+                startingGame();
             }
             else
             {
@@ -118,11 +194,24 @@ public class playerMove : MonoBehaviour
             Instantiate(corona, new Vector2(12, yCorona), transform.rotation);
         }
     }
-    /*void destroyAll(string tag){
+    void destroyAll(string tag){
         GameObject[] enemies = GameObject.FindGameObjectsWithTag(tag);
         for (int i = 0; i < enemies.Length; i++)
         {
             Destroy(enemies[i]);
         }
+    }
+
+    void startingGame()
+    {
+        thePlayer.velocity = new Vector2(0, 1) * jumpForce * Time.deltaTime;
+        if (transform.position.y >= 0)
+        {
+            gameStart = false;
+            StartCoroutine(theCoroutine());
+            theStatus.text = "Start";
+            //Debug.Log("Start");
+        }
     }*/
 }
+
---
+++ Saviour Robot/Assets/veryEasy/player.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class player : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Hello");
+    }
+    /*
+    [Header("Reference")]
+    public Rigidbody2D  thePlayer;
+    public TextMeshProUGUI theStatus;
+    public GameObject drone;
+    public GameObject corona;
+    public GameObject[] theHeart;
+
+    [Header("Value")]
+    public int life;
+    public float jumpForce;
+    public bool play;
+    
+
+    void Start()
+    {
+        thePlayer = GetComponent<Rigidbody2D>();
+        life=5;
+        startGame();
+    }
+
+    
+    void Update()
+    {
+        if(play)
+        {
+            if((Input.GetMouseButtonDown(0) || Input.GetButtonDown("Jump")))
+            {
+                thePlayer.velocity = new Vector2(0, 1) * jumpForce * Time.deltaTime;   
+            }
+            if(transform.position.y>5||transform.position.y<-5)
+            {
+                minusLife();
+            }
+        }
+    }
+    void minusLife()
+    {
+        life--;
+        if(life<1)
+        {
+            Destroy(theHeart[0].gameObject);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }else if(life<2)
+        {
+            Destroy(theHeart[1].gameObject);
+        }else if(life<3)
+        {
+            Destroy(theHeart[2].gameObject);
+        }else if(life<4)
+        {
+            Destroy(theHeart[3].gameObject);
+        }else if(life<5)
+        {
+            Destroy(theHeart[4].gameObject);
+        }
+        startGame();
+    }
+
+    void startGame()
+    {
+        StopCoroutine(theCoroutine());
+        theStatus.text="System reboot";
+        while(transform.position.y<=0)
+        {
+            thePlayer.velocity = new Vector2(0,1)*jumpForce*Time.deltaTime;
+            play=true;
+            StartCoroutine(theCoroutine());
+        }
+    }
+    IEnumerator theCoroutine()
+    {
+        while(life>=0)
+        {
+            float timeGap = Random.Range(0.5f,2f);
+            yield return new WaitForSeconds(timeGap);
+
+            float yCorona = Random.Range(3, 8);
+            float yDrone = Random.Range(-4, -8);
+
+            GameObject theDrone = Instantiate(drone, new Vector2(10, yDrone), Quaternion.identity);
+            GameObject theCorona = Instantiate(corona, new Vector2(12, yCorona), Quaternion.identity);
+        }
+    }
+    */
+}
